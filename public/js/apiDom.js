c9bda5214a8f17bd8efe7cfd5a8a3bf012c2b2ae
omar-shaath <omarshaath89@gmail.com>
2021-08-04 16:54:41 +0300

    Search by enter key Relates #26

--- public/js/apiDom.js
+++ public/js/apiDom.js
@@ -2,19 +2,29 @@ const imgContainer = document.querySelector('.img-div');
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.querySelector('.searchBtn');
 const getImg = (CounteryName) => {
-  fetch(' https://countriesnow.space/api/v0.1/countries/flag/images', (data) => {
-    for (let i = 0; i < data.data.length; i += 1) {
-      if (data.data[i].name === CounteryName) {
-        imgContainer.textContent = '';
-        const createImg = document.createElement('img');
-        imgContainer.appendChild(createImg);
-        createImg.src = data.data[i].flag;
-        createImg.alt = 'flage img';
+  fetch(
+    ' https://countriesnow.space/api/v0.1/countries/flag/images',
+    (data) => {
+      for (let i = 0; i < data.data.length; i += 1) {
+        if (data.data[i].name === CounteryName) {
+          imgContainer.textContent = '';
+          const createImg = document.createElement('img');
+          imgContainer.appendChild(createImg);
+          createImg.src = data.data[i].flag;
+          createImg.alt = 'flage img';
+        }
       }
-    }
-  });
+    },
+  );
 };
 searchBtn.addEventListener('click', () => {
   const countery = searchInput.value;
   getImg(countery);
+});
+
+searchInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const countery = searchInput.value;
+    getImg(countery);
+  }
 });
