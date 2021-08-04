5342403d08d40c2621177fb40ed3c607f0df8c56
ameera <ameeraabedalnaser2021@gmail.com>
2021-08-04 10:19:56 +0300

    fix-debuge-inHandler Related #9

--- src/handlers.js
+++ src/handlers.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 //handel html page 
 const handelHtml = (response) => {
     const filePath = path.join(__dirname, '..', 'public', 'index.html');
-    fs.readFile(path, (erro, file) => {
+    fs.readFile(filePath, (erro, file) => {
         if (erro) {
             response.writeHead(500, { 'content-Type': 'text/html' });
-            response.write('<h1>server error</h1>')
+            response.write('<h1>server error</h1>');
             response.end();
         } else {
             response.writeHead(200, { 'content-Type': 'text/html' });
@@ -17,27 +17,24 @@ const handelHtml = (response) => {
 }
 
 // handel extention page 
-const handleExtentiion = (response, endpoint) => {
-    const extention = endpoint.split('.')[1];
-    const extensionType = {
-        css: 'text/css',
-        js: 'application/javascript',
-        png: 'image/png',
-        html: 'text/html',
-        jpeg: 'image/jpeg',
-        jpg: 'image/jpg',
-    };
-    const filepath = path.join(__dirname, '..', 'public', );
-    fs.readFile(filepath, (error, file) => {
+const handleSearch = (response, endpoint) => {
+    const queryEndPoint = endpoint.split('?')[1];
+    const { query } = querystring.parse(queryEndPoint);
+    const filepath = path.join(__dirname, '.', 'words.txt');
+    fs.readFile(filepath, 'utf8', (error, contents) => {
         if (error) {
             response.writeHead(500, { 'Content-Type': 'text/html' });
-            response.end('<h1>server error.</h1>');
+            response.end('<h1>Internal server error.</h1>');
         } else {
-            response.writeHead(200, { 'content-Type': extensionType[extention] });
-            response.end(file);
+            let regs = process.platform === "win32" ? '\r\n' : '\n'
+            const wordsArr = contents.split(regs);
+            const result = wordsArr.filter((word) => word.startsWith(query)).slice(0, 20)
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify(result));
         }
     });
 };
+
 
 //handel erro
 
